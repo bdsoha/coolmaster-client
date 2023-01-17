a6cc36e74609cc39bb5279b83fa72c969469bf26
Dov Benyomin Sohacheski <b@kloud.email>
2023-01-17 12:54:49 +0000

    wip

--- src/commands/Properties.ts
+++ src/commands/Properties.ts
@@ -2,8 +2,15 @@ import { BaseCommand }                     from './BaseCommand'
 import { Mode, Speed }                     from '../types'
 import { GenericParser, PropertiesParser } from '../parsers'
 
+export type Operation = '-' | '+'
 
 export class Properties extends BaseCommand {
+    protected joinOperations(values: string[], operation: Operation) {
+        return values
+            .map(value => `${operation}${value[0]}`)
+            .join('')
+    }
+
     public all() {
         return this.call('props', PropertiesParser)
     }
@@ -16,21 +23,19 @@ export class Properties extends BaseCommand {
         return this.call('props', GenericParser, [uid, 'visible', this.boolToInt(visible)])
     }
 
-    public speed(uid: string, speeds: Array<Speed>, operation: '-' | '+') {
-        const joined = speeds
-            // @ts-ignore
-            .map(speed => `${operation}${speed[0]}`)
-            .join('')
-
-        return this.call('props', GenericParser, [uid, 'fspeed', joined])
+    public speed(uid: string, speeds: Array<Speed>, operation: Operation) {
+        return this.call(
+            'props',
+            GenericParser,
+            [uid, 'fspeed', this.joinOperations(speeds as string[], operation)]
+        )
     }
 
-    public mode(uid: string, modes: Array<Mode>, operation: '-' | '+') {
-        const joined = modes
-            // @ts-ignore
-            .map(mode => `${operation}${mode[0]}`)
-            .join('')
-
-        return this.call('props', GenericParser, [uid, 'mode', joined])
+    public mode(uid: string, modes: Array<Mode>, operation: Operation) {
+        return this.call(
+            'props',
+            GenericParser,
+            [uid, 'mode', this.joinOperations(modes as string[], operation)]
+        )
     }
 }
